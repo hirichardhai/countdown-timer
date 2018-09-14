c3d9c9f09506357b2d4fb3ccb9fc89ed32300173
hirichardhai <hirichardhai@gmail.com>
2018-09-13 23:18:06 -0700

    imported app.css and styled our app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (    
       <div className="App">
-        <div className="App-title">
+        <div className="App-title">Countdown to December 25, 2017</div>
         <div>
           <div className="Clock-days">14 days</div>
           <div className="Clock-hours">30 hours</div>
@@ -12,9 +13,8 @@ class App extends Component {
           <div className="Clock-seconds">20 seconds</div>
         </div>
         <div>
-          <input placeHolder="new date" />
+          <input placeholder="new date" />
           <button>Submit</button>
-        </div>
         </div>
       </div>
     )
