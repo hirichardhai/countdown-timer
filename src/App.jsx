de139e3df7f911593bfe3edf72f0be1558250dde
hirichardhai <hirichardhai@gmail.com>
2018-09-13 23:13:23 -0700

    added overall structure and format for time countdown

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (    
-      <div>Countdown Champ, App Component</div>
+      <div className="App">
+        <div className="App-title">
+        <div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
+        </div>
+        <div>
+          <input placeHolder="new date" />
+          <button>Submit</button>
+        </div>
+        </div>
+      </div>
     )
   }
 }
