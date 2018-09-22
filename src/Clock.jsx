0b680d1f8f0f892e16a2b43631fc81c754ae40b5
hirichardhai <hirichardhai@gmail.com>
2018-09-22 00:33:19 -0700

    added leading0 to help display time more clearly

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -37,10 +37,10 @@ class Clock extends Component {
     render() {
         return (
             <div>
-            <div className="Clock-days">{this.state.days} days</div>
-            <div className="Clock-hours">{this.state.hours} hours</div>
-            <div className="Clock-minutes">{this.state.minutes} minutes</div>
-            <div className="Clock-seconds">{this.state.seconds} seconds</div>
+            <div className="Clock-days">{this.leading0(this.state.days)} days</div>
+            <div className="Clock-hours">{this.leading0(this.state.hours)} hours</div>
+            <div className="Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+            <div className="Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
           </div>
         )
     }
